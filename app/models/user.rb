2f290a0427ccecd0cbb05484b760a020dae4603d
Bryan Allen <bryanallen.socal@gmail.com>
2013-10-16 10:56:49 -0700

    Finalized user rspec and model email validation.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,14 @@
 class User < ActiveRecord::Base
   # Make sure to force lower case emails.
-  before_save { self.email = email.downcase }
+  has_secure_password
+  before_save { email.downcase! }
   validates :name,  presence: true, length: { maximum: 50 }
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
   # email is not case sensitive and requires it to be unique. However there is an added database
   # validation check for sanity because we can only test at the memory level here.
   # If patron submitted twice during POST it is possible for this to pass when it should fail.
   validates :email, presence:   true,
             format:     { with: VALID_EMAIL_REGEX },
             uniqueness: { case_sensitive: false }
-  has_secure_password
   validates :password, length: { minimum: 6 }
 end
