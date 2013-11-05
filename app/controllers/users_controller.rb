6ba7d37900ccaaa5b62b4d03008bf13049be4ea6
Bryan Allen <bryanallen.socal@gmail.com>
2013-11-05 14:31:45 -0800

    Prep for Chapter 10

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,10 +13,20 @@ class UsersController < ApplicationController
   end
 
   def new
+    # Do not let a signed in user create another user.
+    if signed_in?
+      redirect_to root_url
+    end
+
     @user = User.new
   end
 
   def create
+    # Do not let a signed in user create another user.
+    if signed_in?
+       redirect_to root_url
+    end
+
     @user = User.new(user_params)
     if @user.save
       #calling sign_in from sessions_helper.rb. This is global because of
@@ -42,9 +52,15 @@ class UsersController < ApplicationController
   end
 
   def destroy
-    User.find(params[:id]).destroy
-    flash[:success] = "User deleted."
-    redirect_to users_url
+    user = User.find(params[:id])
+    #current_user is already an admin otherwise he would not make it to this point.
+    if current_user !=  user
+      user.destroy
+      flash[:success] = "User deleted."
+      redirect_to users_url
+    else
+      redirect_to(root_url)
+    end
   end
 
   private
